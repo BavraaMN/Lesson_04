30e4790105f0275e0db6e595da854ce01ac6dee7
Мурат Бауржан <Murat_B@itc.capital.kz>
2022-03-28 18:27:22 +0600

    тест

---
+++ einstein.js
@@ -0,0 +1,4 @@
+﻿const myWeight=120;
+const speedLight=3e8;
+
+console.log(`энергии вашего тела ${myWeight*speedLight * speedLight} джоуль`); 
---
+++ script.js
@@ -0,0 +1,33 @@
+﻿
+         
+  
+
+      const producnameconst = prompt("Наименование товара?", "");
+      console.log('Наименование товара?: ', producnameconst);
+
+
+      const totalproductcountconst = prompt("Количество товара (должен только цифры)?", "");
+      console.log('Количество товара ?: ', totalproductcountconst);
+
+      const productcategoryconst = prompt("Категория товара?", "");
+      console.log('Категория товара?: ', productcategoryconst);
+
+
+      const productcostconst = prompt("Цена товара ? (должен только цифры) ?", "");
+      console.log('Цена товара ?: ', productcostconst);
+
+
+      if (isNaN(totalproductcountconst))
+      {
+         console.log('Количество товара только цифры надо')
+      }      
+         else    if (isNaN(productcostconst))
+      {
+         console.log('Цена товара только цифры надо')
+      }      
+      else 
+      {                             
+       console.log(`На складе ${totalproductcountconst} единицы товара "${producnameconst}" (${productcategoryconst}) на сумму ${totalproductcountconst*productcostconst} деревянных`);
+      }
+
+  
